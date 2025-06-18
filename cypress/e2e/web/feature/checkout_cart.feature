3e9a39a0dcaf72c5e80d6a74fbdea367adc1fa3c
Julio <djuliocmartins@gmail.com>
2025-06-18 16:36:34 -0300

    fix: ajuste teste e versao

--- cypress/e2e/web/feature/checkout_cart.feature
+++ cypress/e2e/web/feature/checkout_cart.feature
@@ -1,5 +1,6 @@
 Feature: Validar produtos no checkout
 
   Scenario: Validar itens adicionados ao carrinho
-    Given Eu acesso o carrinho de compras
+    Given Que loguei "testefirst@teste.com.br" e a senha "testefirst"
+    When Eu acesso o carrinho de compras
     Then Devo visualizar os produtos que adicionei
--- cypress/support/step_definitions/checkout_cart.steps.js
+++ cypress/support/step_definitions/checkout_cart.steps.js
@@ -1,9 +1,16 @@
-import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('Eu acesso o carrinho de compras', () => {
+Given('Que loguei {string} e a senha {string}', (email, senha) => {
+  cy.visit('/login');
+  cy.get('input[data-qa="login-email"]').type(email);
+  cy.get('input[data-qa="login-password"]').type(senha);
+  cy.get('button[data-qa="login-button"]').click();
+});
+
+When('Eu acesso o carrinho de compras', () => {
   cy.visit('/view_cart');
 });
 
 Then('Devo visualizar os produtos que adicionei', () => {
-  cy.get('.cart_info').should('be.visible');
+  cy.get('#product-1').should('be.visible');
 });
